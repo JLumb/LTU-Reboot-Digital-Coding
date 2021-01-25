f7b09d565144279136a40a41646727eb3b5ad871
JLumb <77274799+JLumb@users.noreply.github.com>
2021-01-25 11:06:26 +0000

    Update task-3.1.4.js
    add 2 var

---
+++ module-3/lesson-1/scripts/task-3.1.4.js
@@ -0,0 +1,15 @@
+var name='neenoscompwins'
+
+var name ='lumbscompwins'
+
+var neenoscompwins = 1;
+
+var lumbscompwins = 9;
+
+var compwinner1 = lumbscompwins - neenoscompwins;
+
+var compwinner2 = lumbscompwins % neenoscompwins;
+
+document.write(compwinner1);
+
+alert(compwinner2)
