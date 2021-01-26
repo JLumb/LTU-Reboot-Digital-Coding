456eee9543bcd4063782eff81a835f6eb15a9373
JLumb <77274799+JLumb@users.noreply.github.com>
2021-01-26 10:00:26 +0000

    Update task-3.2.1.js
    add function

---
+++ module-3/lesson-2/scripts/task-3.2.1.js
@@ -0,0 +1,13 @@
+//declare a function
+function neenosstats () {
+
+    document.write('neenos statistics are a lot higher than his gameplay seems to be capable of')
+    document.write('the above statement leads people to think he is cheating')
+}
+
+
+//invoking fuction
+
+neenosstats();
+
+
