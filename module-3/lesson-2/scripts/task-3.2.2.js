f63848e43961c3232673ca0ed8cd25dc2f452d1d
JLumb <77274799+JLumb@users.noreply.github.com>
2021-01-26 10:33:43 +0000

    Update task-3.2.2.js

---
+++ module-3/lesson-2/scripts/task-3.2.2.js
@@ -0,0 +1,9 @@
+function showfullname (fName,sName){
+
+    var fullname = fName + sName;
+
+    document.write(fName + sName)
+}
+
+showfullname ('Joshua', 'Lumb');
+showfullname ('Anthony', 'Brown')
