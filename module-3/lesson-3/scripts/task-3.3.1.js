c569463b26bc131534dea221e1985c79a5a90dc3
JLumb <77274799+JLumb@users.noreply.github.com>
2021-01-27 10:11:08 +0000

    Update task-3.3.1.js
    add 2 arguments
    and return results

---
+++ module-3/lesson-3/scripts/task-3.3.1.js
@@ -0,0 +1,9 @@
+function percentageCalculator (number,percentage) {
+
+    let percentageOf = ( number * percentage)/100;
+    return percentageOf;
+}
+
+console.log (percentageCalculator (200 , 2));
+console.log (percentageCalculator (500, 15));
+
