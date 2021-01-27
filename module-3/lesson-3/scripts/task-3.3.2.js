928a29af89846abd91c848e40a1f5b50b2d73c66
JLumb <77274799+JLumb@users.noreply.github.com>
2021-01-27 10:41:45 +0000

    Update task-3.3.2.js
    add size

--- module-3/lesson-3/scripts/task-3.3.2.js
+++ module-3/lesson-3/scripts/task-3.3.2.js
@@ -2,17 +2,17 @@ function drinkOrder (size,drink) {
     switch (drink) {
 
         case 'Coke':
-            console.log('You have ordered a ' + drink);
+            console.log('You have ordered a ' + size +' '+ drink);
             break;
 
 
         case 'Fanta':
-            console.log('You have ordered a ' + drink);
+            console.log('You have ordered a ' + size +' '+ drink);
             break;
 
         
         case 'Sprite':
-            console.log('you have ordered a ' + drink);
+            console.log('you have ordered a ' + size +' '+ drink);
             break;
 
         default:
