0cb85aa5a7e360e19378796187c3a24701f38a43
JLumb <77274799+JLumb@users.noreply.github.com>
2021-01-30 16:57:18 +0000

    Update 3.4.3.js
    create array

--- module-3/lesson-4/scripts/3.4.3.js
+++ module-3/lesson-4/scripts/3.4.3.js
@@ -1,3 +1,3 @@
-var favouriteFood = ['sandwich', 'smoothies','oranges','Maccies','apple'];
+var favouriteFood = ['sandwich', 'smoothies','oranges','Maccies','apple','roast dinner'];
 
 console.log(favouriteFood)
