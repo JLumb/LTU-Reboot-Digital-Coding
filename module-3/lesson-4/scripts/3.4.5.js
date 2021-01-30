47772e675b7446ad43f5655f3d541495ddae8680
JLumb <77274799+JLumb@users.noreply.github.com>
2021-01-30 17:17:18 +0000

    Create 3.4.5.js
    link js sheet

---
+++ module-3/lesson-4/scripts/3.4.5.js
@@ -0,0 +1,22 @@
+var favouriteRecipe = {
+
+    recipeTitle:'Lasagna',
+    servings: 4,
+    ingredients: [
+    '4x Pasta sheets', 
+    '500g Mince',
+    '600g tomato sauce',
+    '300G cheese'
+],
+    directions: [
+        'make sauce',
+        'layer pasta',
+        'melt cheese',
+    ],
+    letscook: function (){
+        console.log('im hungry! lets cook')
+    }
+}
+
+
+favouriteRecipe.letscook ();
