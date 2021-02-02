3b20661f002789af8b26ddfeb66c30ecda35a307
JLumb <77274799+JLumb@users.noreply.github.com>
2021-02-02 10:52:46 +0000

    Update 3.6.1.js
    add let for logo and header
    with selector to edit each

---
+++ module-3/lesson-6/scripts/3.6.1.js
@@ -0,0 +1,6 @@
+let logo = document.querySelector('img');
+    logo.setAttribute('src', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRA6lC3cyVhSpwVkzRVOSyre_DVPr24ka0VOg&usqp=CAU');
+
+
+let header = document.querySelector('h1');
+    header.style.color = 'green';
